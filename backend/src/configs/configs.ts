c83705f3516ecda0bc2310641e68d44e747c3906
noahbuilds <edetnoah@gmail.com>
2023-04-02 14:11:35 +0100

    fix: security fix

--- backend/src/configs/configs.ts
+++ backend/src/configs/configs.ts
@@ -8,7 +8,9 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const configuration  = {
     ENV_PORT : process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    USER_EMAIL: process.env.USER_EMAIL,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
 }
 
 
--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,12 +1,13 @@
 import nodemailer from "nodemailer";
+import configuration from "../configs/configs";
 
 const transport = nodemailer.createTransport({
   service: "Gmail",
   port: 587,
   secure: false, // upgrade later with STARTTLS
   auth: {
-    user: "noah@brimble.app",
-    pass: "noahbrimble",
+    user: configuration.USER_EMAIL,
+    pass: configuration.EMAIL_PASSWORD,
   },
 });
 
